3590c8a96541132e82794aea324b64bd83c76ea9
Hélder Filipe <h.filipe11@hotmail.com>
2021-11-11 15:50:23 +0000

    tttt

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(324, 422);
+            this.button1.Location = new System.Drawing.Point(306, 384);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(201, 97);
             this.button1.TabIndex = 0;
