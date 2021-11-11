d4c4a5d7c274d48415263d8bf27511f5ace15f84
danieljvsa <danielviana18@gmail.com>
2021-11-11 17:17:16 +0000

    Base de dados (base)

---
+++ erasmus_manager.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE erasmus_manager
+
+use erasmus_manager
+
+CREATE TABLE Users (
+	userId int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	email varchar(155) NOT NULL UNIQUE,
+	username varchar(155) NOT NULL,
+	password varchar(155) NOT NULL,
+)
+
+CREATE TABLE Projects (
+	projectId int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	projectName varchar(155) NOT NULL,
+	userId int FOREIGN KEY REFERENCES Users(userId) NOT NULL,
+)
+
+CREATE TABLE Partners (
+	partnerId int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	projectId int FOREIGN KEY REFERENCES Projects(projectId) NOT NULL,
+	userId int FOREIGN KEY REFERENCES Users(userId) NOT NULL,
+)
+
+CREATE TABLE Students (
+	studentId int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	studentName varchar(155) NOT NULL,
+	--atributos ainda precisam serem adicionados
+)
+
+CREATE TABLE Teams (
+	teamId int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	projectId int FOREIGN KEY REFERENCES Projects(projectId) NOT NULL,
+	studentId int FOREIGN KEY REFERENCES Students(studentId) NOT NULL,
+)
+
+CREATE TABLE Results (
+	resultId int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	chemistry float(10) NOT NULL,
+	createdAt DATETIME NOT NULL,
+	projectId int FOREIGN KEY REFERENCES Projects(projectId) NOT NULL,
+	userId int FOREIGN KEY REFERENCES Users(userId) NOT NULL,
+)
